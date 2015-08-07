116138638ece433ca8210a8107150ae9cdc0f1e9
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-08-07 13:06:18 +0200

    Setup hello world

---
+++ app/src/main/java/org/faudroids/babyface/app/BabyFaceApp.java
@@ -0,0 +1,67 @@
+package org.faudroids.babyface.app;
+
+
+import android.app.Application;
+
+import org.faudroids.babyface.BuildConfig;
+
+import roboguice.RoboGuice;
+import timber.log.Timber;
+
+public class BabyFaceApp extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// setup DI
+		RoboGuice.getOrCreateBaseApplicationInjector(
+				this,
+				RoboGuice.DEFAULT_STAGE,
+				RoboGuice.newDefaultRoboModule(this));
+
+		// setup logging
+		if (BuildConfig.DEBUG) {
+			Timber.plant(new Timber.DebugTree());
+		} else {
+			throw new UnsupportedOperationException("production logging not configured!");
+			// Fabric.with(this, new Crashlytics());
+			// Timber.plant(new CrashReportingTree());
+		}
+	}
+
+	/*
+	private static final class CrashReportingTree extends Timber.Tree {
+
+		@Override
+		public void e(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void e(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
+		@Override
+		public void w(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void w(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
+
+		@Override
+		protected void log(int priority, String tag, String message, Throwable t) {
+			// nothing to do here
+		}
+
+	}
+	*/
+
+}
---
+++ app/src/main/java/org/faudroids/babyface/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package org.faudroids.babyface.ui;
+
+import org.faudroids.babyface.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity {
+
+	// empty!
+
+}
