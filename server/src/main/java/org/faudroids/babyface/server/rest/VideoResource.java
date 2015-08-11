f33015871db5338d08ad6712e7429da2201b3c32
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-08-11 20:02:45 +0200

    server: add video download option

--- server/src/main/java/org/faudroids/babyface/server/rest/VideoResource.java
+++ server/src/main/java/org/faudroids/babyface/server/rest/VideoResource.java
@@ -10,15 +10,17 @@ import org.faudroids.babyface.server.photo.DriveApiFactory;
 import org.faudroids.babyface.server.video.VideoConversionStatus;
 import org.faudroids.babyface.server.video.VideoManager;
 
+import java.io.File;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.dropwizard.auth.Auth;
 
@@ -47,10 +49,36 @@ public class VideoResource {
 	@Path("/{videoId}/status")
 	public Status getStatus(@Auth User user, @PathParam("videoId") String videoId) {
 		// TODO check user access rights
-		Optional<VideoConversionStatus> statusOptional = videoManager.getStatusForVideo(videoId);
-		if (!statusOptional.isPresent()) throw new NotFoundException();
-		return new Status(statusOptional.get());
+		VideoConversionStatus status = assertValidVideo(videoId);
+		return new Status(status);
 	}
+
+	@GET
+	@Path("/{videoId}/data")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response getVideoData(@Auth User user, @PathParam("videoId") String videoId) {
+		// TODO check user access rights
+
+		// check params
+		VideoConversionStatus status = assertValidVideo(videoId);
+		if (!status.isComplete()) throw RestUtils.createJsonFormattedException("video not yet converted", 404);
+		if (!status.isConversionSuccessful()) throw RestUtils.createJsonFormattedException("video conversion error", 404);
+
+		// return video
+		File videoFile = status.getVideoFile();
+		return Response
+				.ok(videoFile, MediaType.APPLICATION_OCTET_STREAM)
+				.header("Content-Disposition", "attachment; filename=\"out.mp4\"")
+				.build();
+	}
+
+
+	private VideoConversionStatus assertValidVideo(String videoId) {
+		Optional<VideoConversionStatus> statusOptional = videoManager.getStatusForVideo(videoId);
+		if (!statusOptional.isPresent()) throw RestUtils.createNotFoundException();
+		return statusOptional.get();
+	}
+
 
 
 	@JsonInclude(JsonInclude.Include.NON_NULL)
